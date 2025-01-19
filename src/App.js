55c36187046b03b30d3483b8e26ef259dea00ca6
nlevin6 <nlevin6@uwo.ca>
2025-01-18 19:39:09 -0500

    - got rid of mui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import { Box } from '@mui/material';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import './App.css';
@@ -19,69 +18,82 @@ import CalorieCalc from './components/CalorieCalculator/CalorieCalc';
 import AI from './components/AI/AI';
 import FitBit from './components/FitBit/FitBit';
 
-
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [loading, setLoading] = useState(true);  // Add loading state
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      setIsAuthenticated(true);
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setIsAuthenticated(true);
+        }
+        setLoading(false);
+    }, []);
+
+    if (loading) {
+        return null;
     }
-    setLoading(false);  // Set loading to false once the authentication check is done
-  }, []);
 
-  if (loading) {
-    return null;  // Render nothing (or a spinner) while loading
-  }
+    return (
+        <div className="app-container">
+            {isAuthenticated && (
+                <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
+            )}
+            <div className="content-container">
+                <Routes>
+                    <Route path="/" element={isAuthenticated ? <Home /> : <Navigate replace to="/login" />} />
+                    <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate replace to="/login" />} />
+                    <Route
+                        path="/exercise/:id"
+                        element={isAuthenticated ? <ExerciseDetail /> : <Navigate replace to="/login" />}
+                    />
+                    <Route
+                        path="/Calories"
+                        element={isAuthenticated ? <CaloriePage /> : <Navigate replace to="/login" />}
+                    />
+                    <Route
+                        path="/workout"
+                        element={isAuthenticated ? <WorkoutLog /> : <Navigate replace to="/login" />}
+                    />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/login" element={isAuthenticated ? <Navigate replace to="/home" /> : <Login />} />
+                    <Route path="api/users/:id/verify/:token" element={<EmailVerify />} />
+                    <Route
+                        path="/api/workout/user/:email/workouts"
+                        element={isAuthenticated ? <WorkoutLogger /> : <Navigate replace to="/login" />}
+                    />
+                    <Route path="/AI" element={<AI />} />
+                    <Route path="/FitBit" element={<FitBit />} />
 
-  return (
-    <Box className="app-container">
-      {isAuthenticated && <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />}
-      <Box className="content-container">
-        <Routes>
-          <Route path="/" element={isAuthenticated ? <Home /> : <Navigate replace to="/login" />} />
-          <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate replace to="/login" />} />
-          <Route path="/exercise/:id" element={isAuthenticated ? <ExerciseDetail /> : <Navigate replace to="/login" />} />
-          <Route path="/Calories" element={isAuthenticated ? <CaloriePage /> : <Navigate replace to="/login" />} />
-          <Route path="/workout" element={isAuthenticated ? <WorkoutLog /> : <Navigate replace to="/login" />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={isAuthenticated ? <Navigate replace to="/home" /> : <Login />} />
-          <Route path="api/users/:id/verify/:token" element={<EmailVerify />} />
-          <Route path="/api/workout/user/:email/workouts" element={isAuthenticated ? <WorkoutLogger /> : <Navigate replace to="/login" />} />
-          <Route path ="/AI" element= {<AI/>}/>
-          <Route path ="/FitBit" element= {<FitBit/>}/>
-          
-          <Route
-            path="/profile"
-            element={
-              isAuthenticated ? (
-                <ChakraProvider>
-                  <Profile />
-                </ChakraProvider>
-              ) : (
-                <Navigate replace to="/login" />
-              )
-            }
-          />
-          <Route
-            path="/calc"
-            element={
-              isAuthenticated ? (
-                <ChakraProvider>
-                  <CalorieCalc />
-                </ChakraProvider>
-              ) : (
-                <Navigate replace to="/login" />
-              )
-            }
-          />
-        </Routes>
-      </Box>
-      {isAuthenticated && <Footer />}
-    </Box>
-  );
+                    <Route
+                        path="/profile"
+                        element={
+                            isAuthenticated ? (
+                                <ChakraProvider>
+                                    <Profile />
+                                </ChakraProvider>
+                            ) : (
+                                <Navigate replace to="/login" />
+                            )
+                        }
+                    />
+                    <Route
+                        path="/calc"
+                        element={
+                            isAuthenticated ? (
+                                <ChakraProvider>
+                                    <CalorieCalc />
+                                </ChakraProvider>
+                            ) : (
+                                <Navigate replace to="/login" />
+                            )
+                        }
+                    />
+                </Routes>
+            </div>
+            {isAuthenticated && <Footer />}
+        </div>
+    );
 };
 
 export default App;
